a51eb55f2aafadafb229c5fc55f655caf2cbc130
Juan van den Anker <juan@vdanker.net>
2017-07-11 20:13:35 +1200

    added ts

--- dist/bundle.js
+++ dist/bundle.js
@@ -65,24 +65,20 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_lodash___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_lodash__);
 
-
+Object.defineProperty(exports, "__esModule", { value: true });
+var lodash_1 = __webpack_require__(1);
 function component() {
     var element = document.createElement('div');
-
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = __WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello', 'webpack'], ' ');
-
+    element.innerHTML = lodash_1.default.join(['Hello', 'webpack'], ' ');
     return element;
 }
-
 document.body.appendChild(component());
+
 
 /***/ }),
 /* 1 */
